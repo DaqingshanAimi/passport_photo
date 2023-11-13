9b9239f843e99b07718224fdac6758d74163e028
DaqingshanAimi <1401001456@qq.com>
2023-11-13 16:35:37 +0800

    Add files via upload
    初始化

---
+++ utils/variables.py
@@ -0,0 +1,27 @@
+# coding:utf-8
+import logging
+
+LOGGER = logging.getLogger()
+
+COLOR_MAP = {
+    -2: ('blue', "蓝色"),
+    -3: ('red', "红色"),
+}
+SIZE_MAP = {
+    -2: ('s1', "一寸"),
+    -3: ('s2', "二寸"),
+    -4: ('s1x5', "一寸 x 五寸"),
+    -5: ('s2x5', "二寸 x 五寸"),
+    -6: ('s1x6', "一寸 x 六寸"),
+    -7: ('s2x6', "二寸 x 六寸"),
+    -8: ('s5max', "五寸混合"),
+    -9: ('s6max', "六寸混合"),
+}
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
